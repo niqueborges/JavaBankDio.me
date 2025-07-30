8f9e8056a8bf45e79cf15c8d02e7c64333994a33
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2025-07-30 12:42:55 -0300

    :sparkles: docs: update archives

--- JavaBankSystem/src/main/java/br/com/dio/exception/AccountNotFoundException.java
+++ JavaBankSystem/src/main/java/br/com/dio/exception/AccountNotFoundException.java
@@ -1,7 +1,7 @@
 package br.com.dio.exception;
 
 public class AccountNotFoundException extends RuntimeException {
-    public AccountNotFoundException(String message) {
-        super(message);
-    }
+  public AccountNotFoundException(String message) {
+    super(message);
+  }
 }
